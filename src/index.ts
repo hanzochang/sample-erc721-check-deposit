94fccd36691b4b994b187efe4db1e75a42e0b84d
hanzochang <hanzo.chan@gmail.com>
2023-01-24 19:36:21 +0900

    remove unnesessary console.log

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ const getCollection = async (holderAddress: string): Promise<number[]> => {
   // ① Provider　インスタンスを作成する。
   const provider = new ethers.providers.JsonRpcProvider(
     process.env.RPC_URL as string,
-    process.env.NETWORK as string
+    parseInt(process.env.NETWORK as string)
   )
 
   // ② 　Contract インスタンスを作成する。
@@ -50,12 +50,17 @@ const getCollection = async (holderAddress: string): Promise<number[]> => {
 
   // ③ 調べたいホルダーのウォレットの過去のtokenIdの送信イベントログすべて取得する。
   const sentLogs = await contract.queryFilter(
-    contract.filters.Transfer(holderAddress, null)
+    // contract.filters.Transfer(holderAddress, null)
+    contract.filters.Transfer(process.env.DEPOSITED_ON_WALLET_ADDRESS, null)
   )
 
   // ④ 調べたいホルダーのウォレットの過去のtokenIdの受信イベントログすべて取得する。
   const receivedLogs = await contract.queryFilter(
-    contract.filters.Transfer(null, holderAddress)
+    // contract.filters.Transfer(null, holderAddress)
+    contract.filters.Transfer(
+      process.env.TARGET_WALLET_ADDRESS,
+      process.env.DEPOSITED_ON_WALLET_ADDRESS
+    )
   )
 
   // ⑤ ③と④のログを結合し、EventLogを時間が古いものから順に時系列で並べる。
@@ -65,7 +70,6 @@ const getCollection = async (holderAddress: string): Promise<number[]> => {
       (a, b) =>
         a.blockNumber - b.blockNumber || a.transactionIndex - b.transactionIndex
     )
-
   // ⑥ ⑤のログを操作して、調べたいウォレットが最終的に持っている最新のtokenIdを取得する
   const owned = new Set<number>()
 
@@ -86,6 +90,8 @@ const getCollection = async (holderAddress: string): Promise<number[]> => {
 }
 
 // 実行
-getCollection(process.env.YOUR_WALLET_ADDRESS as string).then((result) => {
-  console.log('result', result)
-})
+getCollection(process.env.DEPOSITED_ON_WALLET_ADDRESS as string).then(
+  (result) => {
+    console.log('result', result)
+  }
+)
